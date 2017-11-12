df5ce947899689854201821e3bcb29481bd3b493
tarun7631 <tarungoel7631@gmail.com>
2017-11-12 16:39:21 +0530

    height1

--- TheCodeProject/models.py
+++ TheCodeProject/models.py
@@ -3,4 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Musician(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    instrument = models.CharField(max_length=100)
